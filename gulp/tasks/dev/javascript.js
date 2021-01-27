5f664dc693276f151a8ccdedabe25a33d130d31f
therandomsecurityguy <derek.chamorro@gmail.com>
2021-01-27 16:00:46 -0600

    JS updates

--- gulp/tasks/dev/javascript.js
+++ gulp/tasks/dev/javascript.js
@@ -8,7 +8,7 @@ var scripts = [
     'node_modules/jquery/dist/jquery.js',
     'node_modules/bootstrap/dist/js/bootstrap.js',
     'node_modules/jssocials/dist/jssocials.js',
-    'static/js/castanet.js',
+    'static/js/alphacast.js',
     'node_modules/mediaelement/build/mediaelement-and-player.js',
     'node_modules/mediaelement-plugins/dist//speed/speed.js',
     'node_modules/mediaelement-plugins/dist//speed/speed-i18n.js'
@@ -20,7 +20,7 @@ gulp.task('js-concat', function(){
         .pipe(sourcemaps.init())
         .pipe(concat('concat.js'))
         // .pipe(gulp.dest('dist'))
-        .pipe(rename('castanet-min.js'))
+        .pipe(rename('alphacast-min.js'))
         .pipe(uglify({ keep_fnames: true }))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('static/js'));
